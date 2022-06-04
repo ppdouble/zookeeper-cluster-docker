5bea3a66075f78da7c2fed6ae5c46619a8651c71
NickDong <dncdd@aliyun.com>
2022-06-04 22:16:31 +0800

    add comment to source code

--- src/main/java/watchclient/DataMonitor.java
+++ src/main/java/watchclient/DataMonitor.java
@@ -2,28 +2,46 @@ package watchclient;
 
 import java.util.Arrays;
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.AsyncCallback.StatCallback;
 import org.apache.zookeeper.Watcher.Event;
 import org.apache.zookeeper.data.Stat;
 import org.apache.zookeeper.KeeperException.Code;
 
+/*
+ *  monitors the data and existence of a ZooKeeper node
+ */
 public class DataMonitor {
 
     boolean dead;
     ZooKeeper zooKeeper;
-    String znode;
+    String zNodePath;
     Watcher chainedWatcher;
     DataMonitorListener dataMonitorListener;
     byte[] prevData;
 
-    public DataMonitor(ZooKeeper zooKeeper, String znode,
-                       Watcher chainedWatcher, DataMonitorListener dataMonitorListener) {
+    public DataMonitor(ZooKeeper zooKeeper, String zNodePath,
+                       Watcher chainedWatcher, DataMonitorListener dataMonitorListener) throws InterruptedException, KeeperException {
         this.zooKeeper = zooKeeper;
-        this.znode = znode;
+        this.zNodePath = zNodePath;
         this.chainedWatcher = chainedWatcher;
         this.dataMonitorListener = dataMonitorListener;
-
+        // Get things started by checking if the node exists. We are going
+        // to be completely event driven
+        //zooKeeper.exists(zNodePath, true);
+        zooKeeper.exists(zNodePath, true, (StatCallback)this, null);
+        /*
+        * java.lang.ClassCastException: class watchclient.DataMonitor cannot be cast to
+        * class org.apache.zookeeper.AsyncCallback$StatCallback (watchclient.DataMonitor and
+        * org.apache.zookeeper.AsyncCallback$StatCallback are in unnamed module of loader 'app')
+        * at watchclient.DataMonitor.<init>(DataMonitor.java:28)
+        * at watchclient.Executor.<init>(Executor.java:71)
+        * at watchclient.Executor.main(Executor.java:42)
+        * */
     }
 
+    /*
+     * implemented in Executor
+     */
     public interface DataMonitorListener {
         void exists(byte data[]);
 
@@ -31,21 +49,24 @@ public class DataMonitor {
     }
 
     public void process (WatchedEvent event) {
-        String path = event.getPath();
+        System.out.println("receive event type name: " + event.getType().name());
+        System.out.println("receive event path: " + event.getPath());
+
+        String eventZNodePath = event.getPath();
         if (event.getType() == Event.EventType.None) {
             switch (event.getState()) {
                 case SyncConnected:
 
                     break;
                 case Expired:
-
+                    System.out.println("It's all over ----------- The ZooKeeper session is no longer valid.");
                     dead = true;
                     dataMonitorListener.closing(Code.SESSIONEXPIRED);
                     break;
             }
         } else {
-            if (null != path && path.equals(znode)) {
-                zooKeeper.exists(znode, true, (AsyncCallback.StatCallback) this, null);
+            if (null != eventZNodePath && eventZNodePath.equals(zNodePath)) {
+                zooKeeper.exists(zNodePath, true, (StatCallback) this, null);
 
             }
         }
@@ -57,6 +78,8 @@ public class DataMonitor {
     public void processResult (Code reasonCode, String path, Object ctx, Stat stat) {
         boolean exists;
 
+        // KeeperException.Code
+        // The code first checks the error codes for znode existence
         switch (reasonCode) {
             case OK:
                 exists = true;
@@ -70,13 +93,13 @@ public class DataMonitor {
                 dataMonitorListener.closing(reasonCode);
                 return;
             default:
-                zooKeeper.exists(znode, true, (AsyncCallback.StatCallback) this, null);
+                zooKeeper.exists(zNodePath, true, (StatCallback) this, null);
                 return;
         }
         byte[] b = null;
         if (exists) {
             try {
-                b = zooKeeper.getData(znode, false, null);
+                b = zooKeeper.getData(zNodePath, false, null);
 
             } catch (KeeperException e) {
                 e.printStackTrace();
--- src/main/java/watchclient/Executor.java
+++ src/main/java/watchclient/Executor.java
@@ -1,12 +1,12 @@
 package watchclient;
 
+import io.netty.util.internal.SocketUtils;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.KeeperException.Code;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
 
 import java.io.*;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener {
 
@@ -17,47 +17,81 @@ public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListe
     DataMonitor dataMonitor;
     Process child;
 
+    // use
     public static void main (String[] args)  {
         /*
         if (args.length < 4) {
-            System.err.println("USAGE: Executor hostPort znode filename program [args ...]");
+            System.err.println("USAGE: Executor hostPort zNode filename program [args ...]");
             System.exit(2);
         }
 */
         //String hostPort = args[0];
-        //String znode = args[1];
+        //String zNodePath = args[1];
         //String filename = args[2];
         //String[] exec = new String[args.length - 3];
         String hostPort = "localhost:12181";
-        String znode = "/firstznode";
-        String filename = "abcfile";
+        String zNodePath = "/firstznode5";
+        String filename = "/firstznode5/abcfile";
         String[] execStr = new String[3];
         execStr[0] = hostPort;
-        execStr[1] = znode;
+        execStr[1] = zNodePath;
         execStr[2] = filename;
         //System.arraycopy(args, 3, exec, 0, exec.length);
 
         try {
-            new Executor(hostPort, znode, filename, execStr).run();
+            new Executor(hostPort, zNodePath, filename, execStr).run();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public Executor (String hostPort, String znode, String filename, String execStr[]) throws KeeperException, IOException {
+    /*
+     * contains the main thread and contains the execution logic
+     */
+    public Executor (String hostPort, String zNodePath, String filename, String execStr[]) throws KeeperException, IOException, InterruptedException {
         System.out.println("Executor const ructor");
         this.filename = filename;
         this.execStr = execStr;
+        /*
+         * the address of the ZooKeeper service
+         * the name of a znode - the one to be watched
+         */
+        // maintains the ZooKeeper connection
+        // passes a reference to itself as the Watcher argument in the ZooKeeper constructor
+        System.out.println("LL: passes a reference to itself as the Watcher argument in the ZooKeeper constructor");
         zooKeeper = new ZooKeeper(hostPort, 3000, this);
-        dataMonitor = new DataMonitor(zooKeeper, znode, null, this);
+
+        /*
+         * It fetches the data associated with the znode and starts the executable.
+         * If the znode changes, the client re-fetches the contents and restarts the executable.
+         * If the znode disappears, the client kills the executable.
+         */
+        // passes a reference to itself as DataMonitorListener argument to the DataMonitor constructor
+        System.out.println("LL: passes a reference to itself as DataMonitorListener argument to the DataMonitor constructor");
+        dataMonitor = new DataMonitor(zooKeeper, zNodePath, null, this);
+
     }
 
     public void run () {
+        System.out.println("fbbbbbbbbbbbbbb");
+        try {
+            zooKeeper.create("/ftest3", "ftestdata".getBytes(UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            byte[] bData = zooKeeper.getData("/ftest3", this, null);
+            System.out.println(new String(bData, UTF_8));
+            System.out.println("ftttttttttttt");
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Executor run %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         try {
             synchronized (this) {
                 while (!dataMonitor.dead) {
+                    System.out.println("wwwwwwwwwwwwwwwwbbbbbbbbbbbb");
                     wait();
+                    System.out.println("wwwwwwwwwwwwaaaaaaaaaaaaaaaaa");
                 }
             }
         } catch (InterruptedException e) {
@@ -65,11 +99,22 @@ public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListe
         }
     }
 
+    /*
+    * The Watcher interface is defined by the ZooKeeper Java API. ZooKeeper uses it to communicate back
+    * to its container. It supports only one method, process(), and ZooKeeper uses it to communicates
+    * generic events that the main thread would be interested in, such as the state of the ZooKeeper
+    * connection or the ZooKeeper session.
+    * */
+    // The implementation  of the Watcher interface
     @Override
     public void process(WatchedEvent watchedEvent) {
+
         dataMonitor.process(watchedEvent);
+        System.out.println("receive event >> : " + watchedEvent.getType().name());
+
     }
 
+    //  Executor's implementation of DataMonitorListener.exists()
     @Override
     public void exists(byte[] data) {
         if (null != data) {
@@ -112,6 +157,7 @@ public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListe
         }
     }
 
+    // Executor's implementation of DataMonitorListener.closing
     @Override
     public void closing(Code reasonCode) {
         synchronized (this) {
